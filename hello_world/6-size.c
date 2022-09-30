afb5b1c3be56f1a15b4547ffee3981f4c2766bd6
Jenny <5704@holbertonstudents.com>
2022-09-29 20:03:53 -0700

    Added how to print of various types

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - void says that there's no input arguments
+*
+*Return: will always return 0
+*/
+int main(void)
+{
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+return (0);
+}
+
