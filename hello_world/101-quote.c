fb529bad7ed51ac2fe696324e721fee44fdc6da0
Jenny <5704@holbertonstudents.com>
2022-10-02 03:03:36 -0700

    Added how to print a quote without using printf and puts

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 10);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 return (1);
 }
 
