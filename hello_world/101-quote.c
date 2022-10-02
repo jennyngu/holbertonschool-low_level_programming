c9df7d4acc2b092fdaef90d6c3d48cc44cb3f705
Jenny <5704@holbertonstudents.com>
2022-10-02 03:00:24 -0700

    Added how to print a quote without using printf and puts

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<unistd.h>
+/**
+*main - void says that there's no input arguments
+*
+*Return: will always return 1
+*/
+int main(void)
+{
+ fwrite(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 10);
+return (1);
+}
+
