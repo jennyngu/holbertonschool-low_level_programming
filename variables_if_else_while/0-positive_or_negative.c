a2b9b984c82ee7af5097f07c779249dbc252ce84
Jenny <5704@holbertonstudents.com>
2022-10-02 19:22:55 -0700

    Added how to use if statement to print if a variable is -ve +ve or 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -13,17 +13,17 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("'n' is positive");
+printf(n "is positive");
 }
 else
 {
 if (n < 0)
 {
-printf("'n' is negative");
+printf(n "is negative");
 }
 else
 {
-printf("'n' is zero");
+printf(n "is zero");
 }
 }
 return (0);
