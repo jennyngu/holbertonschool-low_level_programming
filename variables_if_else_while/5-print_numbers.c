4d8daf02764ab875c83d3f87ce7714c50fec5a9d
Jenny <5704@holbertonstudents.com>
2022-10-02 21:44:42 -0700

    First attempt at printing nummber 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - print single digit numbers of base 10, from 0
+*
+*Return: will always return 0
+*/
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%d '\n'", n);
+}
+return (0);
+}
