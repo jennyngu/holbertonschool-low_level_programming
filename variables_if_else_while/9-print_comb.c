3b6bb697940396e30fb79062803d6af211d6a15f
Jenny <5704@holbertonstudents.com>
2022-10-03 02:34:35 -0700

    First attempt at creating a list of numbers with commas using putchar

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*main - printing single digits as a list
+*
+*Return: will always return 0
+*/
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++);
+{
+putchar(i);
+if (i < '9')
+{
+putchar(',');
+}
+else
+{
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
