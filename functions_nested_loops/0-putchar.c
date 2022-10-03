8652f3971c3ee2f1b3de7ff276c8009e8c1cf786
Jenny <5704@holbertonstudents.com>
2022-10-03 16:50:49 -0700

    Changed code to use _putchar (without using printf)

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,6 +6,13 @@
 */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*main - print single digit numbers of base 10, from 0
+*
+*Return: will always return 0
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
