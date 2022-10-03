a03b7ec7aa6097c47da9fec8c7d7861f5f0e5933
Jenny <5704@holbertonstudents.com>
2022-10-02 21:35:46 -0700

    Added continue if statement within the for loop

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 /**
-*main - void says that there's no input arguments
+*main - print out the letters of the alphabet excluding e and q
 *
 *Return: will always return 0
 */
 int main(void)
 {
 int ch;
-for (ch = 'a'; ch != 'e' && ch != 'q' && ch < 'z'; ch++)
+for (ch = 'a'; ch <= 'z'; ch++)
 {
+if (ch == 'e' || ch == 'q')
+{
+continue;
+}
 putchar(ch);
 }
 putchar('\n');
