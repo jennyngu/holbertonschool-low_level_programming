2354da3de71b5119dcb08a0971dbc04e8007966a
Jenny <5704@holbertonstudents.com>
2022-10-03 21:05:53 -0700

    Swapped return and putchar, added extra statement

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,17 +9,23 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar ('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0);
 		_putchar ('0');
+		return (0);
+	}
+	else if (n <0)
+	{
+		_putchar ('-');
+		return (-1);
 	}
 	else
 	{
-		return (-1);
-		_putchar ('-');
+	  	_putchar ('-');
+		return ('/');
+		
 	}
 }
---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_abs -
+ *
+ *Return: will be 0 if successful
+ */
+
+int _abs(int i)
+{  
+  if (i < 0)
+    {
+      return (i *(-1));
+    }
+  else
+    {
+      return (i);
+    }
+}
