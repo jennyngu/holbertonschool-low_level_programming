a300591dbcda8b4b45a6ff66893978fbf15627bd
Jenny <5704@holbertonstudents.com>
2022-10-03 20:10:25 -0700

    Add how to check for alphabetic characters

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#inlcude "main"
+/**
+ *_isalpha - checks for alphabet characters
+ *
+ *@c: is the argument passed into the function ie. is being checked
+ *
+ *Return: return 1 if the c argument is a letter, return 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
