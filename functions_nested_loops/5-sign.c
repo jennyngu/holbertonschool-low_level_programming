5c8f4e04f19fc40f374e1f0925e363a568d2217f
Jenny <5704@holbertonstudents.com>
2022-10-03 22:10:21 -0700

    Fixed betty style

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -17,15 +17,14 @@ int print_sign(int n)
 		_putchar ('0');
 		return (0);
 	}
-	else if (n <0)
+	else if (n < 0)
 	{
 		_putchar ('-');
 		return (-1);
 	}
 	else
 	{
-	  	_putchar ('-');
+		_putchar ('-');
 		return ('/');
-		
 	}
 }
