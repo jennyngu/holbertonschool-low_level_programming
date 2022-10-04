f471851f8f92011b34677bdf0190fc6949f29868
Jenny <5704@holbertonstudents.com>
2022-10-03 17:46:49 -0700

    Added in comments

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+ *main - prints the alphabet
+ *
+ *Return: will return 0 if successful
+ */
 
 void	print_alphabet(void)
 {
 	int i;
-  
+
 	i = 'a';
 	while (i <= 'z')
 	{
@@ -11,4 +16,5 @@ void	print_alphabet(void)
 		i = i + 1;
 	}
 	_putchar('\n');
+	return (0);
 }
