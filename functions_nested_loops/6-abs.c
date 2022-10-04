5db2ead8c4367ab5d7044c76971c95b680f68ffe
Jenny <5704@holbertonstudents.com>
2022-10-03 21:20:38 -0700

    Added how to get absolute value of a input

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- *_abs -
- *
+ *_abs - returns the absolute value of a number
+ *@i: is the input number to be tested
  *Return: will be 0 if successful
  */
 
 int _abs(int i)
-{  
-  if (i < 0)
-    {
-      return (i *(-1));
-    }
-  else
-    {
-      return (i);
-    }
+{
+if (i < 0)
+{
+return (i * (-1));
+}
+else
+{
+return (i);
+}
 }
---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *print_last_digit - prints last digit of a number
+ *Return: returns the value of the last digit
+ */
+
+int print_last_digit(int)
+{
+
+}
