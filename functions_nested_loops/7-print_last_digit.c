2fced78a57e1c3eaeb63f02c0e30c0a0ff63f2b5
Jenny <5704@holbertonstudents.com>
2022-10-03 23:21:39 -0700

    Added how to print the value of the last digit of a number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,18 @@
 #include "main.h"
 /**
  *print_last_digit - prints last digit of a number
+ *@n: is the input number
  *Return: returns the value of the last digit
  */
 
 int print_last_digit(int n)
 {
-  int x;
-  (n = n%10)
+	int x = n % 10;
+
+	if (x < 0)
+	{
+		x = (x * -1);
+	}
+	_putchar('0' + x);
+	return (x);
 }
---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ *jack_bauer -
+ *
+ *Return: returns 0 if successful
+ */
+
+void jack_bauer(void);
