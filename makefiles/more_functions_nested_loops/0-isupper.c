a3c6778fa7425084cb4c482a34737ccfd3b97939
Jenny <5704@holbertonstudents.com>
2022-10-05 01:24:30 -0700

    Added how to checked for uppercase letter using if statement

---
+++ makefiles/more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+ #include "main.h"
+/**
+ *_isupper -
+ *@c: is the input argument to be run through the function
+ *Return: is 1 is argument is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+                return (0);
+        }
+}
