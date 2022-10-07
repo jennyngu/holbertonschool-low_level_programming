45ee0fcc4eaa1776b3042b7e43867f92aa594e1b
Jenny <5704@holbertonstudents.com>
2022-10-06 19:28:17 -0700

    Fixed betty

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	i = 1;
 
-	while(i <= 100)
+	while (i <= 100)
 	{
-		if(i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("Fizzbuzz");
 		}
-		else if(i % 3 == 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if(i % 5 == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
