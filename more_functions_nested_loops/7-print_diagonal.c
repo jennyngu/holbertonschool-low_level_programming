137e3880dbe86b3d79405133024853677a854d74
Jenny <5704@holbertonstudents.com>
2022-10-06 18:49:08 -0700

    Added how to print diagonal lines using while loop

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,33 @@
 #include "main.h"
 /**
- *print-diagonal - prints a diagonal line
+ *print_diagonal - prints a diagonal line
  *
+ *@n: is the number diagonals to be printed
  *
+ *Return: returns nothing (void)
+ */
+
+void print_diagonal(int n)
+{
+	int r;
+	int c;
+
+	c = 0;
+	while (c < n && n > 0)
+	{
+		r = 0;
+		while (r < c)
+		{
+			_putchar(' ');
+			r++;
+		}
+		_putchar('\\');
+		c++;
+		_putchar('\n');
+	}
+	if (c == 0)
+	{
+		_putchar('\n');
+	}
+
+}
