6074f0463ce9fae41433747d184545fe87b80dfd
Jenny <5704@holbertonstudents.com>
2022-10-06 20:12:44 -0700

    Added how to print numbers up to or down to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -9,5 +9,19 @@
 
 void print_to_98(int n)
 {
-  
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+	}
+	printf("98\n");
 }
