351cecbbf7ad498b1de525d24f1ccd5df811dd0b
Jenny <5704@holbertonstudents.com>
2022-10-10 22:00:28 -0700

    Added how to copy a string pointed to by a source pointer (including \0) to a buffer pointed by a destination pointer

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcat - concatenates two strings
+ *@dest: destination where string is appended
+ *@src: source where string comes from
+ *Return: pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	
+}
---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_strcpy - copies a string pointed to by source, including \0, to destination
+ *@dest: pointer parameter, where the string gets copied to
+ *@src: pointer parameter, where string is copied from
+ *Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		/*iterate through i in src and make dest = to it*/
+		dest[i] = src[i];
+		i++;
+	}
+	/*dest[last index] is = \'0'*/
+	dest[i] = '\0';
+	return (dest);
+}
