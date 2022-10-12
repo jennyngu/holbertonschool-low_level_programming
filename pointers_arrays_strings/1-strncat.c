245ddbb5784c22dfe1db86605e1fb2ef952ef640
Jenny <5704@holbertonstudents.com>
2022-10-11 19:45:54 -0700

    Edited comments to pass checker

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -34,7 +34,7 @@ char *_strncat(char *dest, char *src, int n)
 
 	destLength = _strlen(dest);
 	i = 0;
-	/*strncat overwrites null byte of dest, but src[i] cannot = '\0'*/
+	/*strncat overwrites null char of dest. src[i] cant equal null char*/
 	/*stop when n bytes is reached*/
 	while (src[i] != '\0' && i < n)
 	{
