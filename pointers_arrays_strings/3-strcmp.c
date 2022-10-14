44f736ebf453cd96048d9bdeea93fc9f4c5a7ad8
Jenny <5704@holbertonstudents.com>
2022-10-13 19:40:28 -0700

    Find difference of two string and return ascii number

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,23 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
+	int difference;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0')
+	/*indexes of s1 and  s2 arent null char*/
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
+		/*find difference*/
+		if (s1[i] != s2[i])
+		{
+			difference = s1[i] - s2[i];
+			break;
+		}
+		else
+		{
+			difference = s1[i] - s2[i];
+		}
 		i++;
 	}
-	return ((s1[i] + '0' > s2[i] + '0') - (s1[i] + '0' < s2[i] + '0'));
+	return (difference);
 }
