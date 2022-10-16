6542973c925ffe483c780adb67634a733547e4ba
Jenny <5704@holbertonstudents.com>
2022-10-16 05:01:11 -0700

    Added how to print a chessboard when given arrays

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_chessboard - prints chessboard'
+ *@a: array of 8 char
+ *Return: rows and columns printing chess pieces
+ */
+void print_chessboard(char (*a)[8])
+{
+	int rows;
+	int columns;
+
+	rows = 0;
+	while (rows <= 8)
+	{
+		columns = 0;
+		while (columns < 8)
+		{
+			/*print rows first then columns*/
+			_putchar(a[rows][columns]);
+			columns = columns + 1;
+		}
+		_putchar('\n');
+		rows = rows + 1;
+	}
+}
