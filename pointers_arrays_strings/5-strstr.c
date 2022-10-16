4b0ae53a5f879897f7252ec962fde3784a66d68e
Jenny <5704@holbertonstudents.com>
2022-10-16 00:32:44 -0700

    Added how to find a substring within a string accounting for NULL cases

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,31 @@
 #include "main.h"
 #include <stddef.h>
+
+/**
+ *needle_location_in_haystack - finds char or string within another string
+ *@haystack: pointer to a string that may or may not contain needle string
+ *@needle: pointer to a string that needs to be found
+ *Return: 1 is needle is in haystack, 0 if not
+ */
+
+int needle_location_in_haystack(char *haystack, char *needle)
+{
+	int i;
+
+	i = 0;
+	while (haystack[i] == needle[i] && needle[i] != '\0')
+	{
+		i = i + 1;
+	}
+	if (needle[i] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
 /**
  *_strstr - searches through haystack to find needle & return string of needle
  *@haystack: pointer to string
@@ -12,11 +38,15 @@ char *_strstr(char *haystack, char *needle)
 	int i;
 
 	i = 0;
+	if (haystack == NULL || needle == NULL)
+	{
+		return (NULL);
+	}
 	while (haystack[i] != '\0')
 	{
-		if (needle[i] == haystack[i])
+		if (needle_location_in_haystack(&haystack[i], needle) == 1)
 		{
-			return (needle);
+			return (&haystack[i]);
 		}
 		i = i + 1;
 	}
