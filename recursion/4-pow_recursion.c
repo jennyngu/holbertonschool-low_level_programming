60a3476fad8791930644612604bf1699095a8be8
Jenny <5704@holbertonstudents.com>
2022-10-17 16:23:14 -0700

    Fixed Betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -6,15 +6,15 @@
  *Return: value of x raised by power of y
  */
 
- int _pow_recursion(int x, int y)
- {
-	 if (y < 0)
-	 {
-		 return (-1);
-	 }
-	 else if (y == 0)
-	 {
-		 return (1);
-	 }
-	 return (x * _pow_recursion(x,  y - 1));
- }
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x,  y - 1));
+}
