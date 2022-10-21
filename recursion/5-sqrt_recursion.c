c4d9654f0c8c9b637e8eb46be3eee5b9cce076d5
Jenny <5704@holbertonstudents.com>
2022-10-20 17:00:15 -0700

    Added a recursive function that returns square root of a number

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *sqrt_rec - returns natural square root of a number
+ *@starting_num:
+ *@n:
+ *Return: starting num if have sqrt, -1 if not
+ */
+int sqrt_rec(int starting_num, int n)
+{
+	if (starting_num * starting_num == n)
+	{
+		return (starting_num);
+	}
+	if (starting_num * starting_num > n)
+	{
+		return (-1);
+	}
+	return (sqrt_rec(starting_num + 1, n));
+}
+
+/**
+ *_sqrt_recursion - returns natural square root of a number
+ *@n: int parameter to find the square root of
+ *Return: natural square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+        return (sqrt_rec(1, n));
+}
