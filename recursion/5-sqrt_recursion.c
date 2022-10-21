6e609adbbd9c7dd7e24542556ccd96422e0c3f0a
Jenny <5704@holbertonstudents.com>
2022-10-20 17:03:24 -0700

    Fixed Betty

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  *sqrt_rec - returns natural square root of a number
- *@starting_num:
- *@n:
+ *@starting_num: num start counting from till and if n is reached
+ *@n: input num, num to be reached
  *Return: starting num if have sqrt, -1 if not
  */
 int sqrt_rec(int starting_num, int n)
@@ -26,5 +26,5 @@ int sqrt_rec(int starting_num, int n)
 
 int _sqrt_recursion(int n)
 {
-        return (sqrt_rec(1, n));
+	return (sqrt_rec(1, n));
 }
