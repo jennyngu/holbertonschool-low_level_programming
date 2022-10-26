8a6395f2103b5b178f0c859659670cac386e3fe2
Jenny <5704@holbertonstudents.com>
2022-10-26 01:35:48 -0700

    Removed memset. Set each address to 0 inside function

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,29 +3,6 @@
 #include <stdlib.h>
 
 /**
- *_memset - fills first n bytes of memory pointed by s with constant byte
- *@s: pointer to memory area
- *@b: is the constant byte
- *@n: number of bytes to be filled
- *Return: pointer to the memory area
- */
-
-unsigned *_memset(unsigned *s, unsigned int b, unsigned int n)
-{
-	unsigned int i;
-
-	i = 0;
-	/*iterate through while index is < than n bytes*/
-	while (i < n)
-	{
-		/*replace element at each index with b*/
-		s[i] = b;
-		i = i + 1;
-	}
-	return (s);
-}
-
-/**
  *_calloc - allocates memory for an array using malloc
  *@nmemb: number of elements to be allocated in the array
  *@size: size of elements
@@ -39,21 +16,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	/*malloc size of unsigned ints * how many nmemb (elements) provided*/
-	pointer = malloc(sizeof(pointer) * nmemb);
+	/*malloc total size of array*/
+	pointer = malloc(nmemb * size);
 	if (pointer == NULL)
 		return (NULL);
-	if (nmemb > 0)
-	{
-		/*use memset to set memory to 0*/
-		_memset(pointer, 0, sizeof(nmemb));
-	}
 	i = 0;
-	/*iterate till max num of elements reached*/
-	/*stop finding addresses when i reaches \0 of strings*/
-	while (i <= nmemb && i != '\0')
+	/*sets memory of each address to 0*/
+	while (i < (nmemb * size))
 	{
-		pointer[i] = size;
+		pointer[i] = 0;
 		i = i + 1;
 	}
 	return (pointer);
