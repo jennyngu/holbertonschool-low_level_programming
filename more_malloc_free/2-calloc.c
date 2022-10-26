a981b2b264f0c76b2cfd8991bd520d700a68be9b
Jenny <5704@holbertonstudents.com>
2022-10-25 23:06:49 -0700

    Used memset to set memory to 0

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,6 +3,29 @@
 #include <stdlib.h>
 
 /**
+ *_memset - fills first n bytes of memory pointed by s with constant byte
+ *@s: pointer to memory area
+ *@b: is the constant byte
+ *@n: number of bytes to be filled
+ *Return: pointer to the memory area
+ */
+
+unsigned *_memset(unsigned *s, unsigned int b, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	/*iterate through while index is < than n bytes*/
+	while (i < n)
+	{
+		/*replace element at each index with b*/
+		s[i] = b;
+		i = i + 1;
+	}
+	return (s);
+}
+
+/**
  *_calloc - allocates memory for an array using malloc
  *@nmemb: number of elements to be allocated in the array
  *@size: size of elements
@@ -20,6 +43,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	pointer = malloc(sizeof(pointer) * nmemb);
 	if (pointer == NULL)
 		return (NULL);
+	if (nmemb > 0)
+	{
+		/*use memset to set memory to 0*/
+		_memset(pointer, 0, sizeof(nmemb));
+	}
 	i = 0;
 	/*iterate till max num of elements reached*/
 	/*stop finding addresses when i reaches \0 of strings*/
