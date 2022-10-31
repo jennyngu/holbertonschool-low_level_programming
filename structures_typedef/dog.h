72031f1a4775cc5aa7c11c056568725f9d33955a
Jenny <5704@holbertonstudents.com>
2022-10-30 20:05:40 -0700

    Defined new type dog_t as new name for type struct dog using typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -15,6 +15,9 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* _DOG_H_ */
