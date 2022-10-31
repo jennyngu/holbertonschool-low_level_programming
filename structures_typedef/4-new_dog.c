f257813636b3bf1126ed238eeef6265ca300c694
Jenny <5704@holbertonstudents.com>
2022-10-31 00:14:53 -0700

    Fixed compilation with main

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,7 +15,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	d = malloc(sizeof(*d));
 	if (d == NULL)
+	{
 		return (NULL);
+		free(d);
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
