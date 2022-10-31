daaec9787f0c6a2bc8297d0a9133c351c7d75dca
Jenny <5704@holbertonstudents.com>
2022-10-30 21:42:35 -0700

    Added else statements to prevent duplicate printing. Added new line chars. Created if statement for when d is NULL

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,11 +11,14 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		exit(0);
 	if (d->name == NULL)
-		printf("Name: (nil)");
-	if (d->owner == NULL)
-		printf("Owner: (nil)");
-	printf("Name: %s\n", d->name);
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else printf("Owner: %s\n", d->owner);
 }
