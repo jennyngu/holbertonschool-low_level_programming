7bff9750a1c2de44b2e23b13a1e26556becd0817
Jenny <5704@holbertonstudents.com>
2022-11-15 14:11:10 -0800

    Leak fix?

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -32,10 +32,13 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = (list_t *)malloc(sizeof(list_t));
-	new_node->str = strdup(str);
-	if (str == NULL)
+	if (new_node == NULL)
 	{
-		free(new_node->str);
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
 		free(new_node);
 		return (NULL);
 	}
