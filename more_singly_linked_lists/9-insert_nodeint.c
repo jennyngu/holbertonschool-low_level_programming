565b6c23f01bf6a52c0f5967aa49798ab9dc9e05
Jenny <5704@holbertonstudents.com>
2022-11-17 02:51:22 -0800

    Fixed inserting node at beginning (idx == 0)

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ *insert_nodeint_at_index - inserts node at any given position
+ *@head: pointer to head node
+ *@idx: index where node should be added
+ *@n: integer to be added to list
+ *Return: address of the new node. NULL if failed
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node;
@@ -12,6 +20,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (current_node != NULL)
 	{
 		if (idx == 1)
