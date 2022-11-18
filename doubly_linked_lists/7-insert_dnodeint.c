4c228085a298fcf495db4d41e08dfdf2a8b21bff
Jenny <5704@holbertonstudents.com>
2022-11-17 22:23:58 -0800

    Updated case of adding to the end of list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -20,20 +20,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 	if (idx == 0)
 	{
-		new_node->prev = NULL;
-		new_node->next = *h;
 		if (*h != NULL)
 		{
 			(*h)->prev = new_node;
 		}
+		new_node->next = *h;
 		*h = new_node;
 		return (new_node);
 	}
 	ptr = *h;
 	i = 0;
-	while (ptr != NULL && i != idx)
+	while (ptr != NULL && i != idx - 1)
 	{
 		ptr = ptr->next;
 		i = i + 1;
@@ -41,6 +42,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (ptr == NULL)
 	{
 		return (NULL);
+	}
+	if (i == idx - 1)
+	{
+		ptr->next = new_node;
+		new_node->prev = ptr;
+		return (new_node);
 	}
 	ptr->prev->next = new_node;
 	new_node->prev = ptr->prev;
