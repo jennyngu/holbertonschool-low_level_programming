7e0b829c55ad36cba24eae6e51058d33af44c8eb
Jenny <5704@holbertonstudents.com>
2022-11-18 18:43:24 -0800

    Changed to while loop

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,33 +1,28 @@
 #include "lists.h"
 
 /**
- *get_nodeint_at_index - gets nth node of a listint_t linked list
- *@head: pointer to head of list
- *@index: index of node (starting at 0)
- *Return: nth node of listint_t list
+ *get_nodeint_at_index - transverses through a list to find the nth node
+ *@head: pointer to head node
+ *@index: nth node to be found
+ *Return: nth node or NULL if it doesn't exist
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *current_node;
+	unsigned int i;
+	listint_t *ptr;
 
-	current_node = head;
-	if (head == NULL)
+	i = 0;
+	ptr = head;
+	while (head != NULL)
 	{
-		return (NULL);
-	}
-	else if (index == 1)
-	{
-		head = current_node->next;
-		return (head);
-	}
-	else
-	{
-		while (index != 1)
+		if (i == index)
 		{
-			current_node = current_node->next;
-			index = index - 1;
+			return (ptr);
 		}
-		return (current_node->next);
+		ptr = head->next;
+		head = ptr;
+		i = i + 1;
 	}
+	return (NULL);
 }
