b12ef1fa710a808bd703b083ce6bca31f17c7941
Jenny <5704@holbertonstudents.com>
2022-11-24 00:15:21 -0800

    Added a function that returns the number of bits you would need to flip to get from one number to another

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *get_bit - gets value of a bit at given index
+ *@n: integer argument
+ *@index: index to get bit of n integer
+ *Return:  value of the bit at index index or -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int found_bit;
+
+	if (index > 32)
+	{
+		return (-1);
+	}
+	found_bit = (n >> index) & 1;
+	return (found_bit);
+}
+
+/**
+ *flip_bits - flips a bit, changing the number to another
+ *@n: number to be flipped
+ *@m: target number after flip
+ *Return: flipped number
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num;
+	unsigned int i;
+
+	num = 0;
+	i = 0;
+	while (i < 32)
+	{
+		if (get_bit(n, i) != get_bit(m, i))
+		{
+			num = num + 1;
+		}
+		i = i + 1;
+	}
+	return (num);
+}
