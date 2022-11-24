a61c75f83b34708a705c6f0dc0dfc26ca97d0517
Jenny <5704@holbertonstudents.com>
2022-11-23 18:42:29 -0800

    Set the index of array to NULL

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,10 +1,17 @@
 #include "hash_tables.h"
 
+/**
+ *hash_table_create - creates a hash table
+ *@size: size of has table
+ *Return: pointer to new has table or NULL if failed
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned long int i;
 
-	new_table = (hash_table_t *) malloc(sizeof(new_table));
+	new_table = malloc(sizeof(*new_table));
 	if (new_table == NULL)
 	{
 		return (NULL);
@@ -13,7 +20,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(new_table->array) * size);
 	if (new_table->array == NULL)
 	{
+		free(new_table);
 		return (NULL);
+	}
+	i = 0;
+	while (i < new_table->size)
+	{
+		new_table->array[i] = NULL;
+		i = i + 1;
 	}
 	return (new_table);
 }
