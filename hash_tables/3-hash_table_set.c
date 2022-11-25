2b09990425e13c35473d0d6b303c85bea534bbe2
Jenny <5704@holbertonstudents.com>
2022-11-24 22:07:34 -0800

    Fixed compilation of c files

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,21 +11,26 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_item;
-	int hash_index;
+	unsigned long int hash_index;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL)
 	{
 		return (0);
 	}
-	new_item = malloc(sizeof(new_item));
+	new_item = malloc(sizeof(*new_item));
+	if (new_item == NULL)
+	{
+		return (0);
+	}
+	hash_index = key_index((const unsigned char *)key, ht->size);
 	new_item->key = strdup(key);
 	new_item->value = strdup(value);
-	hash_index = key_index((const unsigned char *)key, ht->size);
-	while (ht->array[hash_index] != NULL && ht->array[hash_index]->key != NULL)
+	while (ht->array[hash_index] != NULL)
 	{
 		hash_index = hash_index + 1;
-		hash_index = hash_index % (ht->size);
+		hash_index = hash_index % ht->size;
 	}
+	new_item->next = ht->array[hash_index];
 	ht->array[hash_index] = new_item;
 	return (1);
 }
